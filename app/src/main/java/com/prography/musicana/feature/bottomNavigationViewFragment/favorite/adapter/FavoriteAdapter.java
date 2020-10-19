313298c775f714370431222641a0aed1a0e6580a
sohiyp199716 <sohiyp199716@gmail.com>
2020-10-19 14:38:46 +0300

    19/10  complet Deigne ArchivesFragment and FavoriteFragment

---
+++ app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/favorite/adapter/FavoriteAdapter.java
@@ -0,0 +1,35 @@
+package com.prography.musicana.feature.bottomNavigationViewFragment.favorite.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.prography.musicana.R;
+
+public class FavoriteAdapter extends RecyclerView.Adapter<FavoriteAdapter.MyHolder> {
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View root= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_phone_list,parent,false);
+        return  new MyHolder(root);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 7;
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
