f116afcac418e54201b00b1a0eaf5ec2eec0dad0
wejdan <jeejmm99@gmail.com>
2020-10-22 17:33:32 +0300

    map custom info window

--- app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/home/mapFragment/MapFragment.java
+++ app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/home/mapFragment/MapFragment.java
@@ -1,22 +1,25 @@
 package com.prography.musicana.feature.bottomNavigationViewFragment.home.mapFragment;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.libraries.maps.CameraUpdateFactory;
 import com.google.android.libraries.maps.GoogleMap;
-import com.google.android.libraries.maps.GoogleMapOptions;
 import com.google.android.libraries.maps.OnMapReadyCallback;
 import com.google.android.libraries.maps.SupportMapFragment;
+import com.google.android.libraries.maps.model.BitmapDescriptorFactory;
 import com.google.android.libraries.maps.model.LatLng;
-import com.google.android.libraries.maps.model.MapStyleOptions;
+import com.google.android.libraries.maps.model.Marker;
 import com.google.android.libraries.maps.model.MarkerOptions;
 import com.prography.musicana.R;
 
@@ -49,11 +52,56 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng latLng = new LatLng(-34, 151);
+        LatLng latLngObj = new LatLng(31.283598, 34.252791);
         MarkerOptions markerOptions = new MarkerOptions()
-                .position(latLng)
-                .title("wejdan");
+                .position(latLngObj)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_marker_default))
+                .anchor(0.5f, 0.5f);
         mMap.addMarker(markerOptions);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLngObj));
+
+        LatLng latLngObj2 = new LatLng(40.283598, 28.252791);
+        MarkerOptions markerOptions2 = new MarkerOptions()
+                .position(latLngObj2)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_marker_default))
+                .anchor(0.5f, 0.5f);
+        mMap.addMarker(markerOptions2);
+
+        mMap.setOnInfoWindowClickListener(marker -> {
+            Toast.makeText(getActivity(), "gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg", Toast.LENGTH_SHORT).show();
+            System.out.println("wejdan is so mad");
+        });
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            // Use default InfoWindow frame
+            @Override
+            public View getInfoWindow(Marker marker) {
+                View v = getLayoutInflater().inflate(R.layout.map_custom_info, null);
+
+                TextView title = v.findViewById(R.id.map_custom_info_title);
+                TextView name = v.findViewById(R.id.map_custom_info_name);
+                TextView location = v.findViewById(R.id.map_custom_info_location);
+                title.setText("wejdan murad");
+
+                return v;
+            }
+
+            // Defines the contents of the InfoWindow
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                // Getting view from the layout file info_window_layout
+                View v = getLayoutInflater().inflate(R.layout.map_custom_info, null);
+                return v;
+            }
+
+        });
+//        LatLng latLng = new LatLng(-34, 151);
+//        MarkerOptions markerOptions2 = new MarkerOptions()
+//                .position(latLng)
+//                .title("wejdan");
+//        mMap.addMarker(markerOptions2);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
     }
 }
