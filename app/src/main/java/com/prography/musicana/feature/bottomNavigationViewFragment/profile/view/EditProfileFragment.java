91605dac430ecdd039e5602d1f5ab8339b46d3e7
wejdan <jeejmm99@gmail.com>
2021-02-03 18:34:13 +0300

    wrong things

--- app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/profile/view/EditProfileFragment.java
+++ app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/profile/view/EditProfileFragment.java
@@ -1,32 +1,77 @@
 package com.prography.musicana.feature.bottomNavigationViewFragment.profile.view;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.prography.musicana.BuildConfig;
+import com.prography.musicana.R;
 import com.prography.musicana.databinding.FragmentEditProfileBinding;
 import com.prography.musicana.custem.SWInterface.OnFragmentInteractionListener;
 import com.prography.musicana.feature.bottomNavigationViewFragment.profile.model.profiledata.ProfileData;
 import com.prography.musicana.feature.bottomNavigationViewFragment.profile.viewmodel.ProfileViewModel;
 import com.prography.musicana.feature.forgotPassword.ForgotPasswordActivity;
 import com.prography.musicana.utils.SWStaticMethods;
+import com.yalantis.ucrop.UCrop;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 public class EditProfileFragment extends Fragment {
+    private static final int REQUEST_PERMISSION_CODE_CAMERA = 799;
+    private static final int REQUEST_CODE_CAMERA = 999;
+    private static final int REQUEST_PERMISSION_CODE_GALLERY = 788;
+    private static final int REQUEST_CODE_GALLERY = 888;
+
+    private File file;
+    private int count = 0;
+    private Uri outputFileUri = null;
+    private static final String TAG = "AddCategoryActivity";
 
     private FragmentEditProfileBinding binding;
     private OnFragmentInteractionListener mListener;
     private ProfileViewModel profileViewModel;
-    public static final String TAG = EditProfileFragment.class.getSimpleName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,13 +93,12 @@ public class EditProfileFragment extends Fragment {
 
         binding.edEmail.setEnabled(false);
         binding.edPassword.setEnabled(false);
-        binding.edEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SWStaticMethods.intentWithoutDataAndFinish(getActivity(), ForgotPasswordActivity.class);
-            }
+        binding.edEmail.setOnClickListener(v -> {
+            SWStaticMethods.intentWithoutDataAndFinish(getActivity(), ForgotPasswordActivity.class);
         });
-
+        binding.editProfileUserImage.setOnClickListener(v -> {
+            chooseUserPhotoFromGallery();
+        });
 
         getdata();
     }
@@ -92,4 +136,193 @@ public class EditProfileFragment extends Fragment {
             }
         });
     }
+
+    public void chooseUserPhotoFromGallery() {
+        try {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION_CODE_GALLERY);
+            } else {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto, REQUEST_CODE_GALLERY);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void chooseUserPhotoFromCamera() {
+        try {
+
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_CODE_CAMERA);
+            } else {
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+
+                if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q) {
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                    String imageFileName = "JPG_" + timeStamp + "_";
+                    ContentValues values = new ContentValues();
+                    values.put(MediaStore.Images.Media.TITLE, imageFileName);
+                    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+                    outputFileUri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                    startActivityForResult(intent, REQUEST_CODE_CAMERA);
+                } else {
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N) {
+                        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                        String imageFileName = timeStamp + ".jpg";
+                        File storageDir = Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES);
+                        String pictureImagePath = storageDir.getAbsolutePath() + "/" + imageFileName;
+//                        file = new File(pictureImagePath);
+
+                        outputFileUri = Uri.fromFile(file);
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+                        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+                    } else {
+                        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                        String imageFileName = "JPEG_" + timeStamp + "_";
+                        //This is the directory in which the file will be created. This is the default location of Camera photos
+                        File storageDir = new File(Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_DCIM), "Camera");
+                        File image = File.createTempFile(
+                                imageFileName,  /* prefix */
+                                ".jpg",         /* suffix */
+                                storageDir      /* directory */
+                        );
+                        // Save a file: path for using again
+                        outputFileUri = Uri.fromFile(image);
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(getContext(), BuildConfig.APPLICATION_ID + ".provider", image));
+
+                        startActivityForResult(intent, REQUEST_CODE_CAMERA);
+                    }
+
+
+                    // cameraFilePath = "file://" + image.getAbsolutePath();
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            //  Toast.makeText(this, R.string.connection_error, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == REQUEST_CODE_CAMERA && resultCode == RESULT_OK) {
+                if (outputFileUri != null) {
+                    Log.e(TAG, "CapturePhoto: " + outputFileUri + "");
+                    String destinationFileName = "DubaiLadPP" + count + ".jpg";
+//                    ivUserAvatar.setImageURI(data.getData());
+                    count++;
+                    Uri destinationUri = Uri.fromFile(new File(getActivity().getCacheDir(), destinationFileName));
+                    UCrop.Options options = new UCrop.Options();
+                    options.setCompressionQuality(50);
+                    options.setCircleDimmedLayer(false);
+                    options.setShowCropFrame(false);
+                    options.setShowCropGrid(false);
+                    options.setToolbarTitle(getString(R.string.crop_image_text));
+                    options.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+                    options.setToolbarColor(getResources().getColor(R.color.colorPrimaryDark));
+                    UCrop.of(outputFileUri, destinationUri)
+                            .withOptions(options)
+                            .withAspectRatio(1, 1)
+                            .start(getActivity());
+                }
+
+            } else if (requestCode == REQUEST_CODE_GALLERY && resultCode == RESULT_OK && data != null) {
+                Uri imageUri = data.getData();
+                if (imageUri != null) {
+                    Log.d(TAG, "Image Uri " + imageUri + "");
+//                    ivUserAvatar.setImageURI(imageUri);
+                    String destinationFileName = "DubaiLadPP" + count + ".jpg";
+                    count++;
+                    outputFileUri = imageUri;
+                    Uri destinationUri = Uri.fromFile(new File(getActivity().getCacheDir(), destinationFileName));
+                    UCrop.Options options = new UCrop.Options();
+                    options.setCircleDimmedLayer(false);
+                    options.setCompressionQuality(100);
+                    options.setToolbarTitle(getString(R.string.crop_image_text));
+                    options.setShowCropFrame(false);
+                    options.setShowCropGrid(false);
+                    options.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+                    options.setToolbarColor(getResources().getColor(R.color.colorPrimaryDark));
+                    UCrop.of(imageUri, destinationUri)
+                            .withOptions(options)
+                            .withAspectRatio(1, 1)
+                            .start(getActivity());
+                }
+            } else if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+                Uri resultUri = UCrop.getOutput(data);
+                if (resultUri != null) {
+
+                    outputFileUri = resultUri;
+                    file = new File(outputFileUri.getPath());
+                    Bitmap bm = BitmapFactory.decodeFile(resultUri.getPath());
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bm.compress(Bitmap.CompressFormat.JPEG, 30, baos);
+                    binding.editProfileUserImage.setImageBitmap(bm);
+                    Glide.with(getContext()).setDefaultRequestOptions(new RequestOptions())
+                            .load(bm).centerCrop().diskCacheStrategy(DiskCacheStrategy.NONE).into(binding.editProfileUserImage);
+                }
+            } else if (resultCode == UCrop.RESULT_ERROR) {
+                final Throwable cropError = UCrop.getError(data);
+                Log.e(TAG, cropError.getMessage());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        try {
+            switch (requestCode) {
+                case REQUEST_PERMISSION_CODE_CAMERA: {
+                    // If request is cancelled, the result arrays are empty.
+                    Log.e("grantResults", grantResults.length + "");
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+                        // permission was granted.
+                        chooseUserPhotoFromCamera();
+                    } else {
+                        Toast.makeText(getContext(), R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+
+                case REQUEST_PERMISSION_CODE_GALLERY: {
+                    // If request is cancelled, the result arrays are empty.
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                        // permission was granted.
+                        chooseUserPhotoFromGallery();
+                    } else {
+                        Toast.makeText(getContext(), R.string.permission_denied, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+                // other 'case' lines to check for other
+                // permissions this app might request.
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
--- app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/profile/view/ProfileFragment.java
+++ app/src/main/java/com/prography/musicana/feature/bottomNavigationViewFragment/profile/view/ProfileFragment.java
@@ -10,25 +10,22 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.prography.musicana.AppConstants;
 import com.prography.musicana.R;
 import com.prography.musicana.SharedPreferencesHelper;
-import com.prography.musicana.databinding.FragmentProfileBinding;
 import com.prography.musicana.custem.SWInterface.OnFragmentInteractionListener;
-import com.prography.musicana.feature.bottomNavigationViewFragment.profile.viewmodel.ProfileViewModel;
-import com.prography.musicana.feature.login.view.LoginActivity;
+import com.prography.musicana.databinding.FragmentProfileBinding;
+import com.prography.musicana.feature.bottomNavigationViewFragment.profile.view.ProfileCustomBottomSheet;
 import com.prography.musicana.feature.onboard.view.PrivacyPolicyActivity;
 import com.prography.musicana.feature.onboard.view.TermsConditionsActivity;
-import com.prography.musicana.utils.SWStaticMethods;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -38,21 +35,14 @@ public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
     private BottomSheetDialog sheetDialog;
     private OnFragmentInteractionListener mListener;
-    private ProfileViewModel profileViewModel;
-    private SharedPreferencesHelper sharedPreferencesHelper;
-    private static final String TAG = ProfileFragment.class.getSimpleName();
 
     @Override
-
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         sheetDialog = new BottomSheetDialog(getActivity());
-        profileViewModel = new ViewModelProvider(getActivity()).get(ProfileViewModel.class);
-        sharedPreferencesHelper = new SharedPreferencesHelper();
         return binding.getRoot();
-
     }
 
     @Override
@@ -70,8 +60,6 @@ public class ProfileFragment extends Fragment {
             ProfileCustomBottomSheet sheet = new ProfileCustomBottomSheet(getActivity(), sheetDialog, new ProfileCustomBottomSheet.BottomSheetListener() {
                 @Override
                 public void onSwitchClicked(int id, boolean checked, SwitchCompat s1, SwitchCompat s2, SwitchCompat s3) {
-                    SharedPreferences.Editor modeEditor = getActivity().getSharedPreferences(AppConstants.Mode, MODE_PRIVATE).edit();
-
                     switch (id) {
                         case 1:
                             System.out.println("switch 1 is " + checked);
@@ -87,10 +75,10 @@ public class ProfileFragment extends Fragment {
                 }
             });
             sheet.openDialog(getResources().getString(R.string.EntryPermit),
-                    getResources().getString(R.string.RunningBackground),
-                    getResources().getString(R.string.EntryPermitAudioFiles),
-                    null,
-                    false);
+                    null, getResources().getString(R.string.RunningBackground),
+                    null, getResources().getString(R.string.EntryPermitAudioFiles),
+                    null, getResources().getString(R.string.Location),
+                    true);
         });
         binding.profileMood.setOnClickListener(v -> {
             //3
@@ -98,6 +86,7 @@ public class ProfileFragment extends Fragment {
                 @Override
                 public void onSwitchClicked(int id, boolean checked, SwitchCompat s1, SwitchCompat s2, SwitchCompat s3) {
                     SharedPreferences.Editor modeEditor = getActivity().getSharedPreferences(AppConstants.Mode, MODE_PRIVATE).edit();
+
                     switch (id) {
                         case 1:
                             s1.setChecked(checked);
@@ -111,7 +100,6 @@ public class ProfileFragment extends Fragment {
                             }
                             modeEditor.apply();
                             modeEditor.commit();
-
                             break;
                         case 2:
                             s1.setChecked(!checked);
@@ -126,14 +114,13 @@ public class ProfileFragment extends Fragment {
                             modeEditor.apply();
                             modeEditor.commit();
                             break;
-
                     }
                 }
             });
             sheet.openDialog(getResources().getString(R.string.Mood),
-                    getResources().getString(R.string.DarkMode),
-                    getResources().getString(R.string.MoonMode),
-                    null,
+                    SharedPreferencesHelper.getMode(getContext()) == 1, getResources().getString(R.string.DarkMode),
+                    SharedPreferencesHelper.getMode(getContext()) == 2, getResources().getString(R.string.MoonMode),
+                    null, null,
                     false);
         });
         binding.profileLanguage.setOnClickListener(v -> {
@@ -153,9 +140,9 @@ public class ProfileFragment extends Fragment {
                 }
             });
             sheet.openDialog(getResources().getString(R.string.Language),
-                    getResources().getString(R.string.English),
-                    getResources().getString(R.string.Arabic),
-                    null,
+                    null, getResources().getString(R.string.English),
+                    null, getResources().getString(R.string.Arabic),
+                    null, null,
                     false);
         });
 
@@ -189,10 +176,10 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-
+        //////********///
         binding.profileLogoff.setOnClickListener(v -> {
-
-            logoutRequest();
+            //log off
+            Toast.makeText(getActivity(), "log off", Toast.LENGTH_SHORT).show();
         });
     }
 
@@ -206,21 +193,5 @@ public class ProfileFragment extends Fragment {
                     + " must implement OnFragmentInteractionListener");
         }
     }
-
-
-    public void logoutRequest() {
-        profileViewModel.logout().observe(getActivity(), logout -> {
-            if (logout != null) {
-                Toast.makeText(getActivity(), "logout done", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onChanged: " + sharedPreferencesHelper.getToken());
-                sharedPreferencesHelper.clerData();
-                SWStaticMethods.intentWithoutData(getActivity(), LoginActivity.class);
-            } else {
-                Toast.makeText(getActivity(), "some thing wrong", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onChanged: " + sharedPreferencesHelper.getToken());
-            }
-        });
-    }
-
 
 }
